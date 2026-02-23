c15bffba066fa43e7270f76f4b2713c93c6711b5
Manuel-Sphe <sphesihlemanuel99@gmail.com>
2026-02-23 09:26:50 +0200

    More Mock Data

--- src/main/java/com/aggregation/api/infrastructure/external/ProviderClientA.java
+++ src/main/java/com/aggregation/api/infrastructure/external/ProviderClientA.java
@@ -26,8 +26,8 @@ public class ProviderClientA {
                 Transaction.of(
                         "22a23bf4-295a-4ba0-ad2d-3ae0751582ac",
                         "cust-1",
-                         "ZoneFitness Claremont",
-                        new BigDecimal("450.00"),"ZAR",
+                        "ZoneFitness Claremont",
+                        new BigDecimal("450.00"), "ZAR",
                         LocalDateTime.of(2026, 1, 2, 10, 15)
                 ),
                 Transaction.of(
@@ -43,6 +43,78 @@ public class ProviderClientA {
                         "Virgin Active Cavendish",
                         new BigDecimal("799.00"), "ZAR",
                         LocalDateTime.of(2026, 1, 5, 17, 30)
+                ),
+
+                // --- added more mock transactions (variety across customers/merchants/dates) ---
+                Transaction.of(
+                        "70fcd3cd-4c4d-4d1c-86d7-0e45e24f3a4b",
+                        "cust-1",
+                        "Woolworths",
+                        new BigDecimal("356.80"), "ZAR",
+                        LocalDateTime.of(2026, 1, 6, 13, 5)
+                ),
+                Transaction.of(
+                        "2db4c7c2-9113-4f41-bc83-0c7a0a6a9150",
+                        "cust-1",
+                        "Netflix",
+                        new BigDecimal("199.00"), "ZAR",
+                        LocalDateTime.of(2026, 1, 7, 0, 2)
+                ),
+                Transaction.of(
+                        "5a3d9a3e-3ee8-4f2d-ae95-15a7f1d7c9fb",
+                        "cust-1",
+                        "Shell Garage",
+                        new BigDecimal("650.00"), "ZAR",
+                        LocalDateTime.of(2026, 1, 8, 7, 42)
+                ),
+                Transaction.of(
+                        "e6a7f4c0-9cdb-4b67-9302-7b77e10ef3e2",
+                        "cust-2",
+                        "Dis-Chem",
+                        new BigDecimal("238.40"), "ZAR",
+                        LocalDateTime.of(2026, 1, 9, 16, 12)
+                ),
+                Transaction.of(
+                        "f0c8c6de-2c59-4b4b-bd06-dca6f2d83c14",
+                        "cust-2",
+                        "Checkers",
+                        new BigDecimal("512.35"), "ZAR",
+                        LocalDateTime.of(2026, 1, 11, 12, 25)
+                ),
+                Transaction.of(
+                        "8a1a4a83-2a6d-4a66-91ce-cc7a1d0d3c2a",
+                        "cust-2",
+                        "Spotify",
+                        new BigDecimal("64.99"), "ZAR",
+                        LocalDateTime.of(2026, 1, 12, 0, 5)
+                ),
+                Transaction.of(
+                        "1b5e7b9f-6a35-4db7-9f4c-52e679d6f10f",
+                        "cust-3",
+                        "Takealot",
+                        new BigDecimal("1299.00"), "ZAR",
+                        LocalDateTime.of(2026, 1, 12, 19, 10)
+                ),
+                Transaction.of(
+                        "9e5f1c45-74d1-4dd7-b5b4-3a5f5c91c7d8",
+                        "cust-3",
+                        "KFC",
+                        new BigDecimal("98.50"), "ZAR",
+                        LocalDateTime.of(2026, 1, 13, 20, 45)
+                ),
+                Transaction.of(
+                        "3f9f1ce0-51aa-4bd3-8d72-6c2d38fe1a93",
+                        "cust-3",
+                        "MyCiti Bus",
+                        new BigDecimal("25.00"), "ZAR",
+                        LocalDateTime.of(2026, 1, 14, 8, 5)
+                ),
+                Transaction.of(
+                        "b624bb69-44be-43b4-9b2c-7fe7b1c9f4a1",
+                        "cust-3",
+                        "Clicks",
+                        new BigDecimal("179.90"), "ZAR",
+                        LocalDateTime.of(2026, 1, 15, 14, 18)
                 )
         );
     }
--- src/main/java/com/aggregation/api/infrastructure/external/ProviderClientB.java
+++ src/main/java/com/aggregation/api/infrastructure/external/ProviderClientB.java
@@ -22,14 +22,14 @@ public class ProviderClientB {
     }
 
     private List<Transaction> mockData() {
-        return List.of(
-                Transaction.of(
-                        "28618d55-82ea-4b3f-b4c8-39befa4f3adc",
-                        "cust-1",
-                        "ZoneFitness Claremont",
-                        new BigDecimal("450.00"),
-                        "ZAR",
-                        LocalDateTime.of(2026, 1, 2, 10, 15)),
+       return List.of(
+               Transaction.of(
+                "28618d55-82ea-4b3f-b4c8-39befa4f3adc",
+                "cust-1",
+                "ZoneFitness Claremont",
+                new BigDecimal("450.00"),
+                "ZAR",
+                LocalDateTime.of(2026, 1, 2, 10, 15)),
                 Transaction.of(
                         "d4a38236-f6e7-4b98-8124-5aba11ccdf72",
                         "cust-1",
@@ -45,8 +45,82 @@ public class ProviderClientB {
                         new BigDecimal("89.99"),
                         "ZAR",
                         LocalDateTime.of(2026, 1, 4, 8, 20)
+                ),
+
+                // --- added more mock transactions (more rows + multiple customers) ---
+                Transaction.of(
+                        "a7e4e0b9-2b2f-4f6a-9fcb-2f7c40d3c85e",
+                        "cust-1",
+                        "Steers",
+                        new BigDecimal("145.70"),
+                        "ZAR",
+                        LocalDateTime.of(2026, 1, 4, 12, 55)
+                ),
+                Transaction.of(
+                        "c2d0f26c-5be6-4f95-a1c7-87c00df644d1",
+                        "cust-1",
+                        "Capitec Bank Fees",
+                        new BigDecimal("7.50"),
+                        "ZAR",
+                        LocalDateTime.of(2026, 1, 6, 9, 1)
+                ),
+                Transaction.of(
+                        "5f2c9a0b-7d62-4a34-9461-6d7f69072836",
+                        "cust-1",
+                        "Mr D Food",
+                        new BigDecimal("268.20"),
+                        "ZAR",
+                        LocalDateTime.of(2026, 1, 7, 19, 33)
+                ),
+                Transaction.of(
+                        "d3b5d11f-1c80-49b7-8c1e-7ac61a6a2e8a",
+                        "cust-2",
+                        "Uber Eats",
+                        new BigDecimal("312.40"),
+                        "ZAR",
+                        LocalDateTime.of(2026, 1, 6, 20, 10)
+                ),
+                Transaction.of(
+                        "8f7b6312-4e0c-47c1-8bb1-7b7b1e95a1d2",
+                        "cust-2",
+                        "Engen 1-Stop",
+                        new BigDecimal("720.00"),
+                        "ZAR",
+                        LocalDateTime.of(2026, 1, 8, 6, 58)
+                ),
+                Transaction.of(
+                        "6b71aedd-8a73-4a23-aad5-7f3ad2d9dc57",
+                        "cust-2",
+                        "Game Store",
+                        new BigDecimal("899.99"),
+                        "ZAR",
+                        LocalDateTime.of(2026, 1, 10, 11, 20)
+                ),
+                Transaction.of(
+                        "c9f03d18-0d8b-4bdf-9d52-0f2c2a4f2b9c",
+                        "cust-3",
+                        "Flight Centre",
+                        new BigDecimal("3499.00"),
+                        "ZAR",
+                        LocalDateTime.of(2026, 1, 9, 15, 44)
+                ),
+                Transaction.of(
+                        "d2c8f95b-57ef-4e4f-95c6-fd6e0dfec9a1",
+                        "cust-3",
+                        "Rain Internet",
+                        new BigDecimal("499.00"),
+                        "ZAR",
+                        LocalDateTime.of(2026, 1, 11, 0, 1)
+                ),
+                Transaction.of(
+                        "2fa0a3c9-88e1-4f33-9430-e6f6c7d1a9c5",
+                        "cust-3",
+                        "Woolworths Cafe",
+                        new BigDecimal("76.30"),
+                        "ZAR",
+                        LocalDateTime.of(2026, 1, 12, 9, 15)
                 )
-        );
+       );
     }
 
 
